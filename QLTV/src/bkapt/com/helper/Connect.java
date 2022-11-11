aef9ee2c028a32db43068037976d3fb177d7a53f
Lussankya <leducthanh45@gmail.com>
2022-11-12 05:00:21 +0700

    project

---
+++ QLTV/src/bkapt/com/helper/Connect.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bkapt.com.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author PC
+ */
+public class Connect {
+    protected Connection con=null;
+    public Connect(){
+        try
+        {
+            
+            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url="jdbc:mysql://localhost:3306/QLTV2";
+            con=DriverManager.getConnection(url,"root","");
+        }
+           catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }   
+    }
+}
